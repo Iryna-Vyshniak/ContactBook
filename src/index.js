bf84e109b546d0ae9a25a34b06c9d6d490b9ac43
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-16 13:23:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
 
 import { theme } from 'styles/theme';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
