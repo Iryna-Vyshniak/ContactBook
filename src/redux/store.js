065d96ba737a7f83efb560a984c96f3ecff77220
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-16 22:36:37 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,49 @@
-import { configureStore } from '@reduxjs/toolkit';
+// import { configureStore } from '@reduxjs/toolkit';
 
-import contactsReducer from './contacts/contacts-slice';
-import filterReducer from './filter/filter-slice';
+// import contactsReducer from './contacts/contacts-slice';
+// import filterReducer from './filter/filter-slice';
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//     filter: filterReducer,
+//   },
+// });
+
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+import { rootReducer } from './root-reducer';
+
+// об'єкт налаштувань, в якому записані, які дані зберігати в Local Storage
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts'],
+  // blacklist: ["filter"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+// дані з локал сторіджа потрапляли відразу в redux під час завантаження
+export const persistor = persistStore(store);
