519c0884b0f8cd9cf681047a2d810e012f4ba7d1
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-16 13:20:05 +0300

    Create SharedLayout.styled.jsx

---
+++ src/modules/SharedLayout.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  max-width: 100%;
+  width: 1200px;
+  margin: 0 auto;
+
+  transition: all 0.25s linear;
+`;
