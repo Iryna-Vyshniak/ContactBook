cd19e4d6d0344757e7c8cf09ef4538cdef08e274
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-16 22:35:28 +0300

    Create auth-operations.js

---
+++ src/redux/auth/auth-operations.js
@@ -0,0 +1,87 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+
+import {
+  userLogin,
+  userSignUp,
+  userLogout,
+  userCurrent,
+} from 'shared/api/authServices';
+
+export const registerUser = createAsyncThunk(
+  'user/register',
+  async (data, { rejectWithValue }) => {
+    try {
+      const result = await userSignUp(data);
+      toast.success('Successfully registered!', {
+        position: 'bottom-right',
+        autoClose: 1500,
+      });
+      console.log('register:', result);
+      return result;
+    } catch ({ response }) {
+      return rejectWithValue(`Ooops! Wrong... Try again or update browser`);
+    }
+  }
+);
+
+export const logInUser = createAsyncThunk(
+  'user/login',
+  async (data, { rejectWithValue }) => {
+    try {
+      const result = await userLogin(data);
+      toast.success('Successfully logged!', {
+        position: 'bottom-right',
+        autoClose: 1500,
+      });
+      console.log('login:', result);
+      return result;
+    } catch (error) {
+      return rejectWithValue(`Ooops! Wrong... Try again or update browser`);
+    }
+  }
+);
+
+export const logOutUser = createAsyncThunk(
+  'user/logout',
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await userLogout();
+      toast.success('Successfully logout!', {
+        position: 'bottom-right',
+        autoClose: 1500,
+      });
+      console.log('logout:', data);
+      return data;
+    } catch (error) {
+      return rejectWithValue(`Ooops! Wrong... Try again or update browser`);
+    }
+  }
+);
+
+export const getCurrentUser = createAsyncThunk(
+  'user/current',
+  async (_, { rejectWithValue, getState }) => {
+    try {
+      const {
+        auth: { token },
+      } = getState();
+
+      const { data } = await userCurrent(token);
+      console.log('currentUser:', data);
+      return data;
+    } catch ({ response }) {
+      return rejectWithValue(`Ooops! Wrong... Try again or update browser`);
+    }
+  },
+  {
+    condition: (_, { getState }) => {
+      const {
+        auth: { token },
+      } = getState();
+      if (!token) {
+        return false;
+      }
+    },
+  }
+);
