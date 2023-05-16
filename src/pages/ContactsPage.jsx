280275ff69bec33636c07ec09c5f501a44124247
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-16 13:22:15 +0300

    Create ContactsPage.jsx

---
+++ src/pages/ContactsPage.jsx
@@ -0,0 +1,19 @@
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Filter } from 'components/Filter/Filter';
+import { Section } from 'components/Section/Section';
+import { Title } from 'components/Title/Title';
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <Section title="PhoneBook">
+      <ContactForm />
+      <Title title="Contacts" />
+      <Filter />
+      <ContactList />
+    </Section>
+  );
+};
+
+export default Contacts;
