107dcb71e82587dbe20e3b3a552f7598f009ed15
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-16 13:23:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,15 @@ import 'react-toastify/dist/ReactToastify.css';
 
 // components
 import { GlobalStyle } from 'styles/GlobalStyle';
-import { Layout } from './Layout/Layout';
-import { Section } from './Section/Section';
-import { Title } from './Title/Title';
-import { Filter } from './Filter/Filter';
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
+import UserRoutes from './UserRoutes';
 
 export const App = () => {
   return (
     <>
-      <Layout>
-        <Section title="PhoneBook">
-          <ContactForm />
-          <Title title="Contacts" />
-          <Filter />
-          <ContactList />
-        </Section>
-        <ToastContainer />
-      </Layout>
+      <UserRoutes />
+
+      <ToastContainer />
+
       <GlobalStyle />
     </>
   );
