53e456482299fa8e869f0fabda7a9efc8119d5b8
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-16 23:37:40 +0300

    update

--- src/redux/contacts/contacts-selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectContact = state => state.contacts.items;
-export const selectIsLoading = state => state.contacts.isLoading;
-export const selectError = state => state.contacts.error;
