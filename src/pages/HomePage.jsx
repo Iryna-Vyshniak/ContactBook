1217c678e1c1bba784cfb4b5bdf6b9e0c458e962
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-16 13:22:36 +0300

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import { Section } from 'components/Section/Section';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <Section title="Welcome to Phonebook!">
+      <p>
+        PhoneBook app is an easy to use contact manager app that gives you
+        facility of saving and viewing your contacts, so that you never lose
+        your contacts. Try it today!
+      </p>
+    </Section>
+  );
+};
+
+export default Home;
