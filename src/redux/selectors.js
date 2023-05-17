6b1e39ec8c9245f7fb4c9c3dcc6d8424698aeccc
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-17 04:50:14 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,10 +1,10 @@
 import { createSelector } from '@reduxjs/toolkit';
 
 // auth
-export const selectToken = ({ auth }) => auth.token;
-export const selectIsLogin = ({ auth }) => auth.isLogin;
-export const selectName = ({ auth }) => auth.user.name;
-export const selectEmail = ({ auth }) => auth.user.email;
+export const selectToken = state => state.auth.token;
+export const selectIsLogin = state => state.auth.isLoggedIn;
+export const selectUser = state => state.auth.user;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
 
 // contacts
 export const selectIsLoading = ({ contacts }) => contacts.isLoading;
@@ -12,7 +12,7 @@ export const selectError = ({ contacts }) => contacts.error;
 export const selectFilter = ({ filter }) => filter;
 
 export const selectContacts = ({ contacts }) =>
-  [...contacts?.items].sort((a, b) => a.name.localeCompare(b.name));
+  [...contacts.items].sort((a, b) => a.name.localeCompare(b.name));
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectFilter],
