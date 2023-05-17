9a6269cacdfabbba60d59c9112525e456b3661a5
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-17 04:49:40 +0300

    Update contactsServices.js

--- src/shared/api/contactsServices.js
+++ src/shared/api/contactsServices.js
@@ -2,17 +2,14 @@ import { authInstance } from './authServices';
 
 export const getAllContacts = () => authInstance.get('/contacts');
 
-export const addContact = data => {
-  return authInstance.post('/contacts', { ...data });
-};
+export const addContact = contact =>
+  authInstance.post('/contacts', { ...contact });
 
-export const deleteContact = id => {
-  return authInstance.delete(`/contacts/${id}`);
-};
+export const deleteContact = id => authInstance.delete(`/contacts/${id}`);
 
-export const editContact = data => {
-  return authInstance.patch(`/contacts/${data.id}`, {
-    name: data.name,
-    number: data.number,
+export const editContact = contact => {
+  return authInstance.patch(`/contacts/${contact.id}`, {
+    name: contact.name,
+    number: contact.number,
   });
 };
