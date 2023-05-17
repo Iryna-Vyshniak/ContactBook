7a03a507023ae38da8c496efe3902b3ff3fecdb1
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-18 00:09:30 +0300

    Update UserMenu.styled.jsx

--- src/modules/UserMenu/UserMenu.styled.jsx
+++ src/modules/UserMenu/UserMenu.styled.jsx
@@ -1,4 +1,6 @@
 import { styled } from 'styled-components';
+import { styled as styles } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -9,3 +11,32 @@ export const Wrapper = styled.div`
     font-weight: 700;
   }
 `;
+
+export const StyledBadge = styles(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
