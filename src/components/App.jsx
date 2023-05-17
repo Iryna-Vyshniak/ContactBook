16c87fbcf1943a9cbdb5dd28c6aef903f1de27d3
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-17 04:53:52 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,22 @@ import 'react-toastify/dist/ReactToastify.css';
 // components
 import { GlobalStyle } from 'styles/GlobalStyle';
 import UserRoutes from './UserRoutes';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getCurrentUser } from 'redux/auth/auth-operations';
+import { useAuth } from 'hooks/useAuth';
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch]);
+
   return (
     <>
-      <UserRoutes />
+      {!isRefreshing && <UserRoutes />}
 
       <ToastContainer />
 
