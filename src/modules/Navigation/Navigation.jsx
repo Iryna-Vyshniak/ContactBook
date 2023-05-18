5230c4c4044667518526a91ba527ed3079bcd11c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-18 22:50:05 +0300

    Update Navigation.jsx

--- src/modules/Navigation/Navigation.jsx
+++ src/modules/Navigation/Navigation.jsx
@@ -1,6 +1,4 @@
-import { Link } from 'react-router-dom';
 import { useAuth } from 'hooks/useAuth';
-import Logo from 'assets/logo-book.png';
 import { Nav, NavLink } from './Navigation.styled';
 
 export const Navigation = () => {
@@ -8,9 +6,6 @@ export const Navigation = () => {
 
   return (
     <Nav>
-      <Link to="/">
-        <img src={Logo} alt="logo" width={48} />
-      </Link>
       <NavLink to="/">Home</NavLink>
       {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
     </Nav>
