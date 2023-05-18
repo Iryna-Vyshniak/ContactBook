768861b09304520c7fcb911d6bd8a684ff461258
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-18 05:42:18 +0300

    Update UserMenu.jsx

--- src/modules/UserMenu/UserMenu.jsx
+++ src/modules/UserMenu/UserMenu.jsx
@@ -2,7 +2,7 @@ import { useDispatch } from 'react-redux';
 import { logOutUser } from 'redux/auth/auth-operations';
 import { StyledBadge, Wrapper } from './UserMenu.styled';
 import { useAuth } from 'hooks/useAuth';
-import { Avatar } from '@mui/material';
+import { Avatar, Chip } from '@mui/material';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
@@ -13,21 +13,28 @@ export const UserMenu = () => {
   return (
     <Wrapper>
       <p>Welcome, {user.name}</p>
-      <StyledBadge
-        overlap="circular"
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        variant="dot"
-      >
-        <Avatar
-          alt="avatar"
-          src={`https://source.unsplash.com/1200x1200?${user.name}`}
-          sx={{ width: 24, height: 24 }}
-        />
-      </StyledBadge>
 
-      <button type="button" onClick={handleLogOut}>
+      <Chip
+        avatar={
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            variant="dot"
+          >
+            <Avatar
+              alt="avatar"
+              src={`https://source.unsplash.com/1200x1200?${user.name}`}
+              sx={{ width: 24, height: 24 }}
+            />
+          </StyledBadge>
+        }
+        label="LogOut"
+        variant="outlined"
+        onClick={handleLogOut}
+      />
+      {/* <button type="button" onClick={handleLogOut}>
         LogOut
-      </button>
+      </button> */}
     </Wrapper>
   );
 };
