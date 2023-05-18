10aa8b345d7cd6ba243fbbd3a8ceec702b1eb9d0
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-18 05:42:02 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,77 @@
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
 import { Section } from 'components/Section/Section';
-import React from 'react';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Advertising } from 'components/Advertising/Advertising';
+
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#608B38',
+    },
+    secondary: {
+      main: '#dee2ff',
+    },
+  },
+});
 
 const Home = () => {
   return (
-    <Section title="Welcome to Phonebook!">
-      <p>
-        PhoneBook app is an easy to use contact manager app that gives you
-        facility of saving and viewing your contacts, so that you never lose
-        your contacts. Try it today!
-      </p>
-    </Section>
+    <ThemeProvider theme={defaultTheme}>
+      <Grid
+        container
+        component="main"
+        sx={{
+          height: '100vh',
+          backgroundImage: `url("https://ik.imagekit.io/irinavn2011/mobile-phone-1572901.jpg?updatedAt=1684181964478")`,
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: t =>
+            t.palette.mode === 'light'
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <CssBaseline />
+        <Grid item xs={false} sm={6} md={7} />
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={5}
+          component={Paper}
+          elevation={24}
+          square
+          sx={{
+            height: '100vh',
+          }}
+        >
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Grid container spacing={2}>
+              <Section title="Welcome to Phonebook!">
+                <Advertising
+                  text="PhoneBook app is an easy to use contact manager app that gives
+                  you facility of saving and viewing your contacts, so that you
+                  never lose your contacts. Try it today!"
+                />
+              </Section>
+            </Grid>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
