b661088c97d3d7da4bad181030f71febae59dab5
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-18 05:42:09 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,17 +1,80 @@
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 import { Section } from 'components/Section/Section';
 import { Title } from 'components/Title/Title';
 
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#608B38',
+    },
+    secondary: {
+      main: '#dee2ff',
+    },
+  },
+});
+
 const Contacts = () => {
   return (
-    <Section title="PhoneBook">
-      <ContactForm />
-      <Title title="Contacts" />
-      <Filter />
-      <ContactList />
-    </Section>
+    <ThemeProvider theme={defaultTheme}>
+      <Grid
+        container
+        component="main"
+        sx={{
+          backgroundImage: `url("https://ik.imagekit.io/irinavn2011/thomas-kolnowski-uY-9Dyz8PPM-unsplash.jpg?updatedAt=1684182727609")`,
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: t =>
+            t.palette.mode === 'light'
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundAttachment: 'fixed',
+        }}
+      >
+        <CssBaseline />
+        <Grid item xs={false} sm={6} md={7} />
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={5}
+          component={Paper}
+          elevation={24}
+          square
+          sx={{
+            height: '100%',
+            background: '#FFF4F0',
+          }}
+        >
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Grid container spacing={2}>
+              <Section title="PhoneBook">
+                <ContactForm />
+                <Title title="Contacts" />
+                <Filter />
+                <ContactList />
+              </Section>
+            </Grid>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
