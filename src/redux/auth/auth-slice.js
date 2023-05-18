9c24f3c632d4f84af55b49629f7cf298d020f482
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-19 02:02:19 +0300

    optimized

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import {
@@ -17,59 +17,79 @@ const initialState = {
   isRefreshing: false,
 };
 
+const customArrThunks = [registerUser, logInUser];
+
+const status = {
+  pending: 'pending',
+  fulfilled: 'fulfilled',
+  rejected: 'rejected',
+};
+
+const fn = status => customArrThunks.map(el => el[status]);
+
+const handlePending = state => {
+  return state;
+};
+
+const handlePendingCurrentUser = state => {
+  state.isRefreshing = true;
+};
+
+const handleRejected = state => {
+  return state;
+};
+
+const handleRejectedCurrentUser = state => {
+  state.isRefreshing = false;
+};
+
+const handleFulfilled = (state, { payload }) => {
+  state.user = payload.user;
+  state.token = payload.token;
+  state.isLoggedIn = true;
+};
+
+const handleFulfilledLogOut = state => {
+  state.user = { name: null, email: null };
+  state.token = null;
+  state.isLoggedIn = false;
+};
+
+const handleFulfilledCurrentUser = (state, { payload }) => {
+  state.user = payload;
+  state.isLoggedIn = true;
+  state.isRefreshing = false;
+};
+
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: builder =>
+  extraReducers: builder => {
+    const { pending, fulfilled, rejected } = status;
     builder
-      .addCase(registerUser.pending, (state, { payload }) => {
-        return state;
-      })
-      .addCase(registerUser.fulfilled, (state, { payload }) => {
-        state.user = payload.user;
-        state.token = payload.token;
-        state.isLoggedIn = true;
-      })
-      .addCase(registerUser.rejected, (state, { payload }) => {
-        return state;
-      })
-      //
-      .addCase(logInUser.pending, (state, { payload }) => {
-        return state;
-      })
-      .addCase(logInUser.fulfilled, (state, { payload }) => {
-        state.user = payload.user;
-        state.token = payload.token;
-        state.isLoggedIn = true;
-      })
-      .addCase(logInUser.rejected, (state, { payload }) => {
-        return state;
-      })
-      //
-      .addCase(logOutUser.pending, (state, { payload }) => {
-        return state;
-      })
-      .addCase(logOutUser.fulfilled, state => {
-        state.user = { name: null, email: null };
-        state.token = null;
-        state.isLoggedIn = false;
-      })
-      .addCase(logOutUser.rejected, (state, { payload }) => {
-        return state;
-      })
-      //
-      .addCase(getCurrentUser.pending, state => {
-        state.isRefreshing = true;
-      })
-      .addCase(getCurrentUser.fulfilled, (state, { payload }) => {
-        state.user = payload;
-        state.isLoggedIn = true;
-        state.isRefreshing = false;
-      })
-      .addCase(getCurrentUser.rejected, (state, { payload }) => {
-        state.isRefreshing = false;
-        //state.error = payload;
-      }),
+      // .addCase(registerUser.pending, handlePending)
+      // .addCase(registerUser.fulfilled, handleFulfilled)
+      // .addCase(registerUser.rejected, handleRejected)
+      // .addCase(logInUser.pending, handlePending)
+      // .addCase(logInUser.fulfilled, handleFulfilled)
+      // .addCase(logInUser.rejected, handleRejected)
+      //.addCase(logOutUser.pending, handlePending)
+      //.addCase(logOutUser.fulfilled, handleFulfilledLogOut)
+      // .addCase(logOutUser.rejected, handleRejected)
+      //.addCase(getCurrentUser.pending, handlePendingCurrentUser)
+      //.addCase(getCurrentUser.fulfilled, handleFulfilledCurrentUser)
+      // .addCase(getCurrentUser.rejected, handleRejectedCurrentUser);
+
+      .addCase(logOutUser.pending, handlePending)
+      .addCase(logOutUser.fulfilled, handleFulfilledLogOut)
+      .addCase(logOutUser.rejected, handleRejected)
+      .addCase(getCurrentUser.pending, handlePendingCurrentUser)
+      .addCase(getCurrentUser.fulfilled, handleFulfilledCurrentUser)
+      .addCase(getCurrentUser.rejected, handleRejectedCurrentUser)
+      .addMatcher(isAnyOf(...fn(pending)), handlePending)
+      .addMatcher(isAnyOf(...fn(fulfilled)), handleFulfilled)
+      .addMatcher(isAnyOf(...fn(rejected)), handleRejected);
+  },
 });
 
 const authPersistConfig = {
