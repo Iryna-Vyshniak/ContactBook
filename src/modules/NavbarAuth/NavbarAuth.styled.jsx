31ddd823c1e2a17841ad18eef4e1f01e81831791
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-18 22:48:23 +0300

    Update NavbarAuth.styled.jsx

--- src/modules/NavbarAuth/NavbarAuth.styled.jsx
+++ src/modules/NavbarAuth/NavbarAuth.styled.jsx
@@ -5,12 +5,13 @@ export const NavLink = styled(Link)`
   padding: 8px 16px;
   border-radius: 4px;
   text-decoration: none;
-  color: black;
+  color: ${props => props.theme.colors.black};
+  text-shadow: ${props => props.theme.shadows.textShadow};
   font-weight: 500;
 
   &.active {
-    color: #608b38;
     font-weight: 700;
-    text-shadow: 0 1px 1px #fff;
+    color: ${props => props.theme.colors.primary};
+    text-shadow: ${props => props.theme.shadows.textShadow};
   }
 `;
