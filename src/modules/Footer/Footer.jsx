58d1968917f0a51154a73cbd69c10e6754622992
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-18 03:27:33 +0300

    Update Footer.jsx

--- src/modules/Footer/Footer.jsx
+++ src/modules/Footer/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { FooterBlock } from './Footer.styled';
 
 const Footer = () => {
   return (
-    <div>
+    <FooterBlock>
       <p>
         Created by
         <a href="https://github.com/Iryna-Vyshniak"> &copy; Iryna V-N </a>{' '}
         {new Date().getFullYear()}
       </p>
-    </div>
+    </FooterBlock>
   );
 };
 
