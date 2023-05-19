c4f737428fe2ce157135bef11d3f228b852e7441
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-19 14:18:50 +0300

    add proptypes

--- src/components/Advertising/Advertising.jsx
+++ src/components/Advertising/Advertising.jsx
@@ -1,5 +1,10 @@
 import { Text } from './Advertising.styled';
+import PropTypes from 'prop-types';
 
 export const Advertising = ({ text }) => {
   return <Text>{text}</Text>;
+};
+
+Advertising.propTypes = {
+  text: PropTypes.string.isRequired,
 };
