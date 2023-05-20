8372f7a7d7b2872bb5d2cbfd186a21a7c062dbbb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-20 12:11:00 +0300

    Update theme.jsx

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -10,7 +10,7 @@ export const theme = {
   space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
   fonts: {
     body: 'system-ui, sans-serif',
-    heading: 'system-ui, sans-serif',
+    heading: 'Lora, cursive',
     monospace: 'Menlo, monospace',
   },
   fontSizes: {
